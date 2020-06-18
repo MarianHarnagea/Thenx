b121e315f484a0401d46d044e8a7603b4985bcaf
Marian Harnagea <marianharnagea.com>
2020-06-18 18:47:03 +0300

    improvemens

--- shop/scripts/shop.js
+++ shop/scripts/shop.js
@@ -36,3 +36,36 @@ closeCartBtn.addEventListener("click", () => {
   body.classList.remove("overflow");
 });
 // Open - Close Cart End
+
+cover.addEventListener("click", () => {
+  cartMenu.classList.remove("open");
+  hamburgerMenu.classList.remove("open");
+  cover.classList.remove("open");
+  body.classList.remove("overflow");
+});
+
+$(document).ready(function () {
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+
+  // when the modal is opened autoplay it
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+
+  // stop playing the youtube video when I close the modal
+  $("#myModal").on("hide.bs.modal", function (e) {
+    // a poor man's stop video
+    $("#video").attr("src", $videoSrc);
+  });
+
+  // document ready
+});
