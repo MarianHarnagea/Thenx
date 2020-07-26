f056c23236a78d3e5d036ea3e01e8d7de3c9cb95
Marian Harnagea <marianharnagea.com>
2020-07-26 21:18:49 +0300

    improvement

--- shop/scripts/shop.js
+++ shop/scripts/shop.js
@@ -7,7 +7,7 @@ const bagBtn = document.querySelector(".bag-btn");
 const closeCartBtn = document.querySelector(".close-cart-btn");
 
 const cover = document.querySelector(".cover");
-const body = document.querySelector("body");
+const body = document.querySelector(".shop-page-container");
 
 // Open - Close Menu
 openMenuBtn.addEventListener("click", () => {
